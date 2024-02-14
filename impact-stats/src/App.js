5646d7ad214f9fc1537b414017e042ec6b03c070
Saskia Hustinx <91604850+sHustinx@users.noreply.github.com>
2024-02-14 16:02:10 +0100

    first version impact stat

--- impact-stats/src/App.js
+++ impact-stats/src/App.js
@@ -1,35 +1,38 @@
-
-import React, { PureComponent } from "react";
+import React, {PureComponent} from "react";
 import {useState, useEffect} from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
+import {ResponsiveContainer} from 'recharts';
+
+
 //Explore more Monday React Components here: https://style.monday.com/
 
 // Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
 const monday = mondaySdk();
 
 const App = () => {
-  const [context, setContext] = useState();
+    const [context, setContext] = useState();
 
 
-  return (
-      <div className="App">
-        <h1 className="text-grey">Average bias <br/>count</h1>
-        <div className="buffer-left-25">
-          <p className="big-num">5</p>
-          <p className="bn-descr text-grey">biased decisions per month</p>
+    return (
+        <div className="App">
+            {/*<ResponsiveContainer width="100%" height="100%"></ResponsiveContainer>*/}
+                <h3 className="text-grey no-margin">Average bias <br/>count</h3>
+                <div className=" align-center">
+                    <p className="big-num">5</p>
+                    <p className="bn-descr text-grey">biased decisions per month</p>
+                </div>
+
+                <div className="spacer-50"></div>
+
+                <h3 className="text-grey no-margin">Average financial <br/>impact</h3>
+                <div className=" align-center">
+                    <p className="big-num">-7.5k €</p>
+                    <p className="bn-descr text-grey">per biased decisions</p>
+                </div>
         </div>
-
-        <div className="spacer-50"></div>
-
-        <h1 className="text-grey">Average financial <br/>impact</h1>
-        <div className="buffer-left-25">
-          <p className="big-num">-7.5k €</p>
-          <p className="bn-descr text-grey">per biased decisions</p>
-        </div>
-      </div>
-  );
+    );
 };
 
 export default App;
