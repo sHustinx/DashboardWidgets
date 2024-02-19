6652bd09f75d7a7a76d4a5eb0a8403e7fa7b7970
Saskia Hustinx <91604850+sHustinx@users.noreply.github.com>
2024-02-19 13:18:03 +0100

    added reconsidered graphic

---
+++ reconsidered-diagram/src/App.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+import {useState, useEffect} from "react";
+import "./App.css";
+import mondaySdk from "monday-sdk-js";
+import "monday-ui-react-core/dist/main.css";
+//Explore more Monday React Components here: https://style.monday.com/
+
+import {PieChart, Pie, Legend, Tooltip, ResponsiveContainer, Cell} from 'recharts';
+
+
+// Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
+const monday = mondaySdk();
+
+const data = [
+    { name: 'not reconsidered', value: 59 },
+    { name: 'reconsidered and unchanged', value: 26 },
+    { name: 'reconsidered and changed', value: 15},
+];
+
+const COLORS = ['#224364', '#497c98', '#79aabe'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.35;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
+class Outcome extends PureComponent {
+
+    render() {
+        return (
+            <ResponsiveContainer width="100%" height="35%">
+                <PieChart width={400} height={200}>
+                    <Pie
+                        data={data}
+                        startAngle={180}
+                        endAngle={0}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        innerRadius={30}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                </PieChart>
+            </ResponsiveContainer>
+        );
+    }
+}
+
+
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <Outcome/>
+        </div>
+    );
+};
+
+export default App;
